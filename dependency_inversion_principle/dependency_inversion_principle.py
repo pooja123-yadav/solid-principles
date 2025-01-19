894a65bfbcd650df608e063f475c096e260f4ba0
pooja123-yadav <85959414+pooja123-yadav@users.noreply.github.com>
2025-01-19 23:27:38 +0530

    Update dependency_inversion_principle.py

---
+++ dependency_inversion_principle/dependency_inversion_principle.py
@@ -0,0 +1,6 @@
+"""
+The Dependency Inversion Principle states that high-level modules must not depend on low-level modules. 
+Instead, both should depend on abstractions. This principle promotes decoupling, meaning concrete classes
+are loosely coupled with high-level classes. To achieve this, classes should depend on interfaces or abstract classes,
+not on concrete classes or functions
+"""
